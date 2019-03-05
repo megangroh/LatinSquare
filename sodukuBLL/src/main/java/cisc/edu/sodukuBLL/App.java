8ffdab499ef2d8e4af7ab9b5db1ddc0412f32930
meg <meg@wifi-roaming-128-4-70-233.host.udel.edu>
2019-03-04 23:08:55 -0500

    initial commit

---
+++ sodukuBLL/src/main/java/cisc/edu/sodukuBLL/App.java
@@ -0,0 +1,13 @@
+package cisc.edu.sodukuBLL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
